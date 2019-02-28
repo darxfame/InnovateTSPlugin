ef1d2eac441e9cbdce7b7e0149da99b55f77405c
Artem <metra52@ya.ru>
2019-03-01 02:12:13 +0300

    Update: universal system adresses

--- InnovatePluginSource/jarm.bat
+++ InnovatePluginSource/jarm.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
-cd D:\Java\projects\innovate_wbo_jar_example\InnovatePluginSource
+cd %~dp0
 del InnovatePlugin.jar
 jar cvmf manifest.mf  InnovatePlugin.jar -C ./bin innovate
 echo "Create"
 echo "Rem last jar"
-del C:\Users\metra\.efianalytics\TunerStudio\plugins\InnovatePlugin.jar
+del %USERPROFILE%\.efianalytics\TunerStudio\plugins\InnovatePlugin.jar
 echo "Copy"
-COPY InnovatePlugin.jar C:\Users\metra\.efianalytics\TunerStudio\plugins\InnovatePlugin.jar
-dir C:\Users\metra\.efianalytics\TunerStudio\plugins\
+COPY InnovatePlugin.jar %USERPROFILE%\.efianalytics\TunerStudio\plugins\InnovatePlugin.jar
+dir %USERPROFILE%\.efianalytics\TunerStudio\plugins\
 echo "=================================SUCCESSFULLY MAKE JAR========================================="
